cafca0d2478656ff928d8278d8aee0c33a691229
Marion darren <d.marion@rt-iut.re>
2026-05-12 02:31:06 +0900

    update form

--- Javascript/contact.js
+++ Javascript/contact.js
@@ -35,11 +35,11 @@ document.querySelector(".card").addEventListener("submit", async (e) => {
   }
 });
 
-/*
+
 const response = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
   method: 'POST',
   body: new URLSearchParams({
     secret: '0x4AAAAAACx8w0sAoiVSQ_6SHpPMfF3KLFI',
     response: token
   })
-});*/
+});
